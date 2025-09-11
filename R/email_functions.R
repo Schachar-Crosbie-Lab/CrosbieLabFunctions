dcce6589665fc19eba32848f380420894e02f30c
jclutton <jonathan.clutton@sickkids.ca>
2025-09-11 11:59:00 -0400

    Added function for building html emails

---
+++ R/email_functions.R
@@ -0,0 +1,141 @@
+#### Send Emails ####
+
+#' @name send_email
+#'
+#' @title Send an Email
+#'
+#' @description It's common in the Crosbie lab to need to send an email as a part of an automated pipeline. R does not provide great functionality for doing so.
+#' This function uses the reticulate package to utilize Python and the smtplib Python package to send emails.
+#'
+#' @param host The host of the server
+#' @param port The port for email sending
+#' @param from Email address of person sending email
+#' @param to The email address or email addresses of people receiving email
+#' @param subject Subject header
+#' @param body The body of the email, which will be in HTML
+#' @param attachments paths to one or multiple files to be included
+#'
+#' @import reticulate
+#'
+#' @return Success of whether email is sent
+#'
+#' @export
+
+
+send_email <- function(host = 'smtp.sickkids.ca', port = 25,
+                       from = NULL, to = NULL, subject = NULL, body = NULL,
+                       attachments = NULL) {
+
+  smtplib_lib <- reticulate::import("smtplib")
+  email_lib <- reticulate::import("email")
+  io_lib <- reticulate::import("io")
+  `%as%` <- reticulate::`%as%`
+
+  # Build email
+  email = email_lib$mime$multipart$MIMEMultipart()
+
+  # Concatenate recipients
+  if(length(to) >= 2){
+    to <- paste(to, collapse = ", ")
+  }
+
+  # Set email parts
+  email["To"] = to
+  email["From"] = from
+  email["Subject"] = subject
+
+  # Body
+  body_format = email_lib$mime$text$MIMEText(body, "html")
+  email_lib$message$Message$attach(email, body_format)
+
+  # Attachments
+  if (!is.null(attachments)) {
+
+    for (file in attachments) {
+      with(io_lib$open(file, "rb") %as% attachment, {
+        part = email_lib$mime$multipart$MIMEBase("application", "octet-stream")
+        email_lib$message$Message$set_payload(
+          part,
+          io_lib$BufferedReader$read(attachment)
+        )
+      })
+
+      email_lib$encoders$encode_base64(part)
+      email_lib$message$Message$add_header(
+        part,
+        "Content-disposition",
+        sprintf("attachment; filename= %s", basename(file))
+      )
+      email_lib$message$Message$attach(email, part)
+    }
+  }
+
+  server = smtplib_lib$SMTP(host, as.integer(port))
+  email = email_lib$message$Message$as_string(email)
+
+  # Try to send the email. Catch the result.
+  result = tryCatch({
+    smtplib_lib$SMTP$sendmail(
+      server,
+      from,
+      reticulate::r_to_py(as.list(to)),
+      email
+    )
+
+    response <- 'Successfully sent email'
+  }, error = function(e){
+    # error handler picks up where error was generated
+    print(paste("Error sending email:  ",e))
+    response <- paste("Error sending email:  ",e)
+  }, finally = function(f){
+    return(response)
+  }
+
+  )
+
+}
+
+#### Email Body Builder ####
+
+#' @name body_builder
+#'
+#' @title Build the body of an HTML email
+#'
+#' @description It's common in the Crosbie lab to need to send an email as a part of an automated pipeline. This function reduces the time to create and HTML body.
+#'
+#' @details
+#' The builder automatically adds the formatting HTML like <tr>, etc. Use the sections param to create the sections in the email. This assumes everything is left aligned.
+#' For more complicated bodies, you'll need to build it yourself or update this function
+#'
+#' @param title metadata title of the email
+#' @param sections Pass the sections of the email into a vector. If you include 5 elements, then 5 sections will be created with a line between each. If you'd like HTML
+#' formatting in the sections, i.e. bolding text, the character text needs to be HTML formatted
+#'
+#' @export
+
+
+body_builder <- function(title = NULL, sections = NULL) {
+
+  header <- paste0('<!DOCTYPE html>
+                  <html lang="en">
+                  <head>
+                  <meta charset="utf-8">
+                  <title><!--',title,'--></title>
+                  </head>')
+
+  # Build body of email
+  body <- character()
+
+  for(i in 1:length(sections)){
+
+    section <- paste0('<tr><td align="left"><tr><p>',sections[i],'</p></tr></td><hr />')
+
+    body <- paste0(body,section)
+  }
+
+  # Full email
+  full <- paste0(header, body)
+
+  return(full)
+
+}
